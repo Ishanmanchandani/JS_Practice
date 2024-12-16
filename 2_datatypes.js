07a316037c01f1a1b3db0a48299491ef0c2d62e3
Ishanmanchandani <159271464+Ishanmanchandani@users.noreply.github.com>
2024-12-16 05:29:53 +0000

    Datatypes

---
+++ 2_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"// treat all js code as newer version
+console.log(3+3);console.log("ishan")
+/*Types of Datatypes
+number= 2 to the power 52
+string
+boolean
+null
+undefined
+symbol= unique
+NON PRIMITIVE:
+object */
+console.log(typeof "ishan")
+console.log(typeof 2)
+console.log(typeof true)
