bad04fc6063008a1cfc0c3ec41f419ea1f0048ec
Ishanmanchandani <159271464+Ishanmanchandani@users.noreply.github.com>
2024-12-17 14:17:27 +0000

    conversion of datatypes

---
+++ 3_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "abc"
+//console.log(typeof (score))
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+let isloggedIn =""
+let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn)
+//1=>true, 0=>false
+//""=>false,  "ishan"=>true
+let someNumber = 23;
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//*********************Operation****************************
+let str1 = "Hello"
+let str2 = "ishan"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log(1 + "2" + 2)
+console.log(1 + 2 + "2")
+ let gameCounter = 100
+ ++gameCounter;
+ gameCounter++;
+ console.log(gameCounter)
