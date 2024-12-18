94190f36beee8cb97774ad4745fc5f1304c3fa47
Ishanmanchandani <159271464+Ishanmanchandani@users.noreply.github.com>
2024-12-18 14:40:05 +0000

    Datatypes Summary

---
+++ 5_datatypesSummary.js
@@ -0,0 +1,24 @@
+//Primitive datatypes
+// String, Number, Boolean,  BigInt, Symbol , null, undefined
+const String =  "ishan"
+const score =100
+const scoreValue= 100.4
+
+const IsLoggedIn = false
+const bigNumber = 45879866963624n
+console.log(bigNumber)
+const outSideTemp = null
+const id =Symbol('123')
+const anotherId = Symbol('123')
+console.log(id==anotherId);
+
+
+
+//Reference (Non Primitive)
+//Array, Object ,Functions
+const  arr = ["mango","apple","banana"]
+let myObj={
+  name:"ishan",
+  age:20  
+}
+console.log(typeof outSideTemp)
