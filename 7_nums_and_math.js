220b19dfaeb246832055b48b9e9536721947799b
Ishanmanchandani <159271464+Ishanmanchandani@users.noreply.github.com>
2024-12-20 08:06:15 +0000

    Nums and Math methods

---
+++ 7_nums_and_math.js
@@ -0,0 +1,23 @@
+const score = 100
+console.log(score);
+const balance = new Number(200)
+
+console.log(balance);
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(3));
+
+const otherNumber = 1122.259
+console.log(otherNumber.toPrecision(6))
+const number= 13200000
+console.log(number.toLocaleString('en-In'))
+
+console.log(Math.max(2,1,4,5));
+
+console.log(Math.random()*10 +1);
+
+console.log(Math.floor(Math.random()*10 + 1));
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min)*10 + min));
+
