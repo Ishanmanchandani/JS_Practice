ab87cb88ea7d8adca699e30f8a641591e98ee375
Ishanmanchandani <159271464+Ishanmanchandani@users.noreply.github.com>
2024-12-23 04:31:26 +0000

    Array js

---
+++ 9_Array.js
@@ -0,0 +1,24 @@
+const arr =[1,2,3,4]
+// console.log(arr[1]);
+// arr.push(5)
+// arr.pop()
+// arr.unshift(0)
+// arr.shift()
+
+
+// console.log(arr.includes(4))
+// console.log(arr.indexOf(4));
+// const newArr = arr.join()
+// console.log(newArr);
+
+console.log("A",arr)
+const myn1 = arr.slice(1,3)
+console.log(myn1);
+console.log("B",arr);
+
+
+const myn2 = arr.splice(1,3)
+console.log("C",arr);
+
+console.log(myn2);
+
