4c110ee359575cc132eb5160dbbd474b519e5626
Ishanmanchandani <159271464+Ishanmanchandani@users.noreply.github.com>
2025-01-01 14:23:09 +0000

    scopes in js

---
+++ 03_basics/2_scopes.js
@@ -0,0 +1,33 @@
+function one() {
+   const username= "ishan"
+   function two() {
+    const platform = "youtube"
+    console.log(username)
+   }
+   //console.log(platform)
+
+   two()
+}
+one()
+
+if (true) {
+ const username = "Ishan"
+ if (username ==="Ishan"){
+    const website = " youtube"
+    console.log( username + website)
+ }
+ //console.log(website)
+    
+}
+//console.log(username)
+console.log(addone(5))
+
+function addone(num){
+   return num + 1
+}
+
+const addtwo = function(num){
+   return num + 2
+} 
+addtwo(5)
+
